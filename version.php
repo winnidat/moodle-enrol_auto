ef9fdf489afef7a0aa1a9559cf7f36346b654132
winnidat <winkler_t@gmx.at>
2017-07-09 19:52:50 +0200

    Update version.php

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017061305;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2017061307;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2015051100;        // Requires this Moodle version
 $plugin->component = 'enrol_auto';      // Full name of the plugin (used for diagnostics)
 $plugin->release = 'hartbeespoort-r2';
